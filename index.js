fadd869c3f6095545003368428e5afd7453b9b04
robertlin <E24026064@mail.naku.edu.tw>
2015-10-12 21:55:02 +0800

    2015/10/12

---
+++ index.js
@@ -0,0 +1,7 @@
+$(function(){
+$('#education').click(function(){ $('html,body').animate({scrollTop:$('#ed').offset().top}, 1000); 
+
+$('#extractivities').click(function(){ $('html,body').animate({scrollTop:$('#extra').offset().top}, 1000); 
+
+$('#skills').click(function(){ $('html,body').animate({scrollTop:$('#ski').offset().top}, 1000); 
+}
