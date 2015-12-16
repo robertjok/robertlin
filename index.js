9be14819c68b8b16e4d6e3104f491e71b48e42d5
robertlin <E24026064@mail.naku.edu.tw>
2015-12-16 17:02:15 +0800

    2015/12/16# Please enter the commit message for your changes. Lines starting

--- index.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-$('#education').click(function(){ $('html,body').animate({scrollTop:$('#ed').offset().top}, 1000); 
-
-$('#extractivities').click(function(){ $('html,body').animate({scrollTop:$('#extra').offset().top}, 1000); 
-
-$('#skills').click(function(){ $('html,body').animate({scrollTop:$('#ski').offset().top}, 1000); 
-}
